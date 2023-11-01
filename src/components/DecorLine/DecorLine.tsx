0e2da9df496d4f08c5c9e7b635b26042a8dc5968
Roman Ahoshton <ro.ahoshton@gmail.com>
2023-11-02 01:41:35 +0200

    Updates

--- src/components/DecorLine/DecorLine.tsx
+++
@@ -1,9 +0,0 @@
-interface forProps{
-    width:number
-}
-
-const DecorLine=({width}:forProps)=>{
-    return<div style={{backgroundColor:"#FF7757",height:3,width:width}}></div>
-}
-
-export default DecorLine
