390329c400bf4be8171110f91f2b27ff3ddef7f5
Roman Ahoshton <ro.ahoshton@gmail.com>
2023-11-11 17:55:18 +0200

    remove condition

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,11 @@ import { CONTENT_PAGE, SIGNIN_PAGE, SIGNUP_PAGE } from "./constants/Routes";
 import { ToastContainer } from "react-toastify";
 
 function App() {
-  const [token, setToken] = useState<boolean>(false);
-  const supabaseTokenKey = "sb-eeqorsxttevsnqgauyjs-auth-token";
-
-  useEffect(() => {
-    const getSupabaseToken = localStorage.getItem(supabaseTokenKey);
-    if (getSupabaseToken) {
-      setToken(true);
-    }
-  }, []);
   return (
     <Router>
       <ToastContainer />
       <Routes>
-        {token ? <Route path={CONTENT_PAGE} element={<ContentPage />} /> : null}
+        <Route path={CONTENT_PAGE} element={<ContentPage />} />
         <Route path={SIGNIN_PAGE} element={<Signin />} />
         <Route path={SIGNUP_PAGE} element={<Signup />} />
       </Routes>
