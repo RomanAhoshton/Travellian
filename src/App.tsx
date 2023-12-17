feadb264c92f53781e09819c5b7946749109a10a
romanahoshton <ro.ahoshton@gmail.com>
2023-12-17 21:09:59 +0200

    aa

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Signup from "./pages/Signup";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { CONTENT_PAGE, SIGNIN_PAGE, SIGNUP_PAGE } from "./constants/Routes";
 import { ToastContainer } from "react-toastify";
-import { useEffect } from "react";
+import { useEffect , memo,} from "react";
 import { useAuth } from "./hooks/useAuth";
 
 const App = () => {
@@ -30,15 +30,19 @@ const App = () => {
       </Router>
     );
   }
-  return (
-    <Router>
-      <ToastContainer />
-      <Routes>
-        <Route path={SIGNIN_PAGE} element={<Signin />} />
-        <Route path={SIGNUP_PAGE} element={<Signup />} />
-      </Routes>
-    </Router>
-  );
+
+
+    return (
+      <Router>
+        <ToastContainer />
+        <Routes>
+          <Route path={SIGNIN_PAGE} element={<Signin />} />
+          <Route path={SIGNUP_PAGE} element={<Signup />} />
+        </Routes>
+      </Router>
+    );
+  
 };
 
-export default App;
+export default memo(App);
+
