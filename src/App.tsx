c8a9a1e2b31c94fc4adfe607151b326e3884e2a6
romanahoshton <ro.ahoshton@gmail.com>
2023-12-17 21:33:29 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import "./App.scss";
 import ContentPage from "./pages/ContentPage";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
-import {  Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { CONTENT_PAGE, SIGNIN_PAGE, SIGNUP_PAGE } from "./constants/Routes";
 import { ToastContainer } from "react-toastify";
-import { useEffect , memo,} from "react";
+import { useEffect, memo } from "react";
 import { useAuth } from "./hooks/useAuth";
 
 const App = () => {
@@ -22,27 +22,19 @@ const App = () => {
 
   if (isAuthenticated) {
     return (
- 
       <Routes>
-          <ToastContainer />
-          <Route path={CONTENT_PAGE} element={<ContentPage />} />
-        </Routes>
-
+        <ToastContainer />
+        <Route path={CONTENT_PAGE} element={<ContentPage />} />
+      </Routes>
     );
   }
 
-
-    return (
- 
-      <Routes>
- ƒ
-          <Route path={SIGNIN_PAGE} element={<Signin />} />
-          <Route path={SIGNUP_PAGE} element={<Signup />} />
-        </Routes>
-
-    );
-  
+  return (
+    <Routes>
+      <Route path={SIGNIN_PAGE} element={<Signin />} />
+      <Route path={SIGNUP_PAGE} element={<Signup />} />
+    </Routes>
+  );
 };
 
 export default memo(App);
-
